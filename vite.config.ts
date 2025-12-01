35398921b99821e7de23ec24d8e6eb2a57bc4ef7
Khushi Roy <khushinroy@gmail.com>
2025-12-01 19:40:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-        allowedHosts: ['https://skilllift-fg4e.onrender.com']
+        allowedHosts: ['skilllift-fg4e.onrender.com']
       },
       plugins: [react()],
       define: {
